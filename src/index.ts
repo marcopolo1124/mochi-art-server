dbea5b52438a35323813467ad9a6fb843ba5d73d
marcopolo1124 <85742840+marcopolo1124@users.noreply.github.com>
2023-02-19 11:17:55 +0000

    changed cors

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ app.use(passport.session())
 app.use(express.json())
 app.use(express.urlencoded( {extended: false} ))
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://miiyachi-art-store.vercel.app/',
   optionsSuccessStatus: 200,
   credentials: true
 }))
